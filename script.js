113127a7afff55d144d2b10528b02c1fd0c0de7f
yinkusadel <adeleyeolayinka73@gmail.com>
2023-08-21 17:54:36 +0100

    add js document

---
+++ script.js
@@ -0,0 +1,46 @@
+let choiceOptions = ['rock', 'paper','scissors']
+let randomChoice = ""
+
+
+let getComputerChoice = () => {
+    for (let i = 0; i < choiceOptions.length; i++) {
+        randomChoice = (choiceOptions[(Math.floor(Math.random() * choiceOptions.length))]);
+      }
+      return randomChoice
+}
+
+
+
+let playerSelection = "rock";
+let computerSelection = getComputerChoice();
+
+let playRound = (playerSelection, computerSelection) => {
+    if (computerSelection == "rock" && playerSelection == "rock") {
+        return "it's a tie"
+      } else if (computerSelection == "scissors" && playerSelection == "rock") {
+        return "you win! rock beats scissors"
+      } else if (computerSelection == "paper" && playerSelection == "rock") {
+        return "You Lose! Paper beats Rock"
+      } else {
+        return "please play again"
+      }
+  }
+
+
+let game = () => {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection));
+      }
+      
+}
+  
+  
+
+
+
+
+
+
+
+
+console.log(      game  ()    )
